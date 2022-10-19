c4e3051ab2b92f7818b0e50304ee41b27b2ec085
akki14192 <ajoshi@odysseussolutions.com>
2022-10-19 02:41:49 -0400

    Added DTO - data transfer object and validation.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,8 +5,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -19,14 +21,16 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register (string username, string password)
+        public async Task<ActionResult<AppUser>> Register (RegisterDto registerDto)
         {
+            if (await IsUserExsist(registerDto.username)) return BadRequest("Username is taken");
+
             using var hmac =  new HMACSHA512();
             
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -34,6 +38,11 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> IsUserExsist(string username)
+        {
+            return await _context.Users.AnyAsync(u => u.UserName == username.ToLower());
         }
     }
 }
