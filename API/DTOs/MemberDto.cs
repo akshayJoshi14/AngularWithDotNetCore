ce262abfa94a04f69384a9dca5d9688c00264b3c
akki14192 <ajoshi@odysseussolutions.com>
2022-11-03 02:26:57 -0400

    Added AutoMapper.

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -9,7 +9,9 @@ namespace API.DTOs
     {
         public int Id { get; set; }
 
-        public string UserName { get; set; }
+        public string Username { get; set; }
+
+        public string PhotoUrl { get; set; }
 
         public int Age { get; set; }
 
