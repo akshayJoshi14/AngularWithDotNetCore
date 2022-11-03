3393be178f79fe38103f4584d7feaf751928ae56
akki14192 <ajoshi@odysseussolutions.com>
2022-11-03 02:52:57 -0400

    Added changes with AutoMapper queryable extensions.

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
@@ -11,8 +14,10 @@ namespace API.Data
     public class UserRepository : IUserRepository
     {
         public DataContext _context { get; }
-        public UserRepository(DataContext context)
+        public IMapper _mapper { get; set; }
+        public UserRepository(DataContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
         }
 
@@ -43,6 +48,21 @@ namespace API.Data
         public void Update(AppUser user)
         {
             _context.Entry(user).State = EntityState.Modified;
+        }
+
+        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        {
+            return await _context.Users
+            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+            .ToListAsync();
+        }
+
+        public async Task<MemberDto> GetMemberAsync(string username)
+        {
+           return await _context.Users
+            .Where(u => u.UserName == username)
+            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+            .SingleOrDefaultAsync();
         }
     }
 }
