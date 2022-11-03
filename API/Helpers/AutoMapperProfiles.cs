23b5d0f97aa65f4a93378cd25346fe366642bf07
akki14192 <ajoshi@odysseussolutions.com>
2022-11-03 02:27:58 -0400

    Added AutoMapper configuration where property name doesn't match.

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -12,7 +12,9 @@ namespace API.Helpers
     {
         protected AutoMapperProfiles()
         {
-            CreateMap<AppUser, MemberDto>();
+            CreateMap<AppUser, MemberDto>()
+            .ForMember (dest => dest.PhotoUrl, opt => opt.MapFrom(src => 
+                src.Photos.FirstOrDefault(x => x.IsMain).Url));
     
             CreateMap<Photo, PhotoDto>();
         }
