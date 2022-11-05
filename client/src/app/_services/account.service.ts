60980b31a444adef2f3f1fadde96bd18b8aedce8
akki14192 <ajoshi@odysseussolutions.com>
2022-11-05 12:56:51 -0400

    Added membercard and memberlist loading.

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  bsseUrl = 'https://localhost:7223/api/';
+  bsseUrl = environment.apiUrl;
 
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+// temp way
+// const httpOptions ={
+//   headers: new HttpHeaders({
+//      Authorization : 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token 
+//   })
+// }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers(){
+    // temp way
+    //return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+
+    return this.http.get<Member[]>(this.baseUrl + 'users')
+  }
+
+  getmember(username: string){
+    //temp way
+    //return this.http.get<Member>(this.baseUrl + 'users/'+ username, httpOptions);
+
+    return this.http.get<Member>(this.baseUrl + 'users/'+ username);
+  }
+
+}
