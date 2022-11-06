eac5891160a1c9392c31795b9a33f27ad416bde1
akki14192 <ajoshi@odysseussolutions.com>
2022-11-06 11:19:48 -0500

    remaining changes with last commit.

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -33,4 +33,9 @@ export class MembersService {
     return this.http.get<Member>(this.baseUrl + 'users/'+ username);
   }
 
+  updateMember(member:Member)
+  {
+    return this.http.put(this.baseUrl + 'users', member);
+  }
+
 }
