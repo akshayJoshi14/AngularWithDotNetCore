624322eefe35d9007058dc5376be757c533b9d10
akki14192 <ajoshi@odysseussolutions.com>
2022-11-08 01:37:01 -0500

    Setting up main photo in the navbar.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -50,7 +50,9 @@ namespace API.Controllers
          [HttpPost("login")]
          public async Task<ActionResult<UserDto>> Login(loginDto loginDto)
          {
-            var user = _context.Users.FirstOrDefault(x=> x.UserName == loginDto.username);
+            var user = _context.Users
+            .Include(p => p.Photos)
+            .FirstOrDefault(x=> x.UserName == loginDto.username);
 
             if(user == null)
             {
@@ -69,7 +71,8 @@ namespace API.Controllers
             return new UserDto
             {
                     Username = user.UserName,
-                    Token = _tokenService.CreateToken(user)
+                    Token = _tokenService.CreateToken(user),
+                    PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
 
          }
