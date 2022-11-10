3f940aa7dc06b1694f4c760a09448310b5e77549
akki14192 <ajoshi@odysseussolutions.com>
2022-11-10 01:29:54 -0500

    Added Reactive forms for regsiter page.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
@@ -12,20 +13,32 @@ export class RegisterComponent implements OnInit {
   //@Input() usersFromHomeComponent : any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
+
   constructor(private accountService : AccountService, private router: Router,
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
+ this.IntializeForm()
+  }
+
+  IntializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello',Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword : new FormControl('',Validators.required)
+    })
   }
 
    register(){
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
-    } )
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe(response => {
+    //   console.log(response);
+    //   this.cancel();
+    // }, error => {
+    //   console.log(error);
+    //   this.toastr.error(error.error);
+    // } )
    }
 
    cancel(){
