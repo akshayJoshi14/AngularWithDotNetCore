1360e8e42bafe75bc27d648c8e4688b831c4e0e7
akki14192 <ajoshi@odysseussolutions.com>
2022-11-14 01:30:16 -0500

    Added like entity and implemented controllers for the likes feature

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourcUserId, int likedUserId);
+
+        Task<AppUser> GetUserWithLikes(int userId);
+
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+    }
+}
