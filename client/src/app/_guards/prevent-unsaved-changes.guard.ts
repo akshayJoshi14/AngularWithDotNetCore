e4b0f6881d328f55f0a78e27932cb0188fa9f86b
akki14192 <ajoshi@odysseussolutions.com>
2022-12-29 11:51:22 -0500

    Getting the data from the confirmation popup.

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(component: MemberEditComponent): boolean {
+  constructor(private confrimSerivce : ConfirmService) {}
+  canDeactivate(component: MemberEditComponent): Observable<boolean> {
       if(component.editForm?.dirty){
-        return confirm('Are you sure you want to countinue? Any unsaved changes will be lost')
+        return this.confrimSerivce.confirm()
       }
-    return true;
+    return of(true);
   }
 }
